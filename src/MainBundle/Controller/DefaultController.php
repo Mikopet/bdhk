1c37c3e1ee9fa5ab02e76fd5e5b030bdc05f2bc5
Mikola Péter <mikopet@gmail.com>
2015-11-08 16:45:05 +0100

    more template integrating

--- src/MainBundle/Controller/DefaultController.php
+++ src/MainBundle/Controller/DefaultController.php
@@ -9,11 +9,47 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/", name="index")
      * @Template()
      */
     public function indexAction()
     {
-        return array('name' => '');
+        return array();
+    }
+
+    /**
+     * @Route("/one", name="onecolumn")
+     * @Template()
+     */
+    public function onecolumnAction()
+    {
+        return array();
+    }
+
+    /**
+     * @Route("/two1", name="twocolumn1")
+     * @Template()
+     */
+    public function twocolumn1Action()
+    {
+        return array();
+    }
+
+    /**
+     * @Route("/two2", name="twocolumn2")
+     * @Template()
+     */
+    public function twocolumn2Action()
+    {
+        return array();
+    }
+
+    /**
+     * @Route("/three", name="threecolumn")
+     * @Template()
+     */
+    public function threecolumnAction()
+    {
+        return array();
     }
 }
